464362fc0b2b34896bda62ccf616b5e71a68e27b
gap579137 <blocklistproject@outlook.com>
2020-07-28 10:35:14 -0500

    Updated some syntax
    Co-Authored-By: null <gap579137@users.noreply.github.com>

--- dedup.py
+++ dedup.py
@@ -1,20 +1,20 @@
 import os
-os.rename(r'lists/crypto.txt',r'lists/crypto-pre.txt')
+os.rename(r'lists/scam.txt',r'lists/scam-pre.txt')
 # This program opens file
 # input file bar.txt and removes duplicate lines and writes the
 # contents to
 # output file foo.txt file.
 lines_seen = set()  # holds lines already seen
-outfile = open('lists/crypto.txt', "w")
-infile = open('lists/crypto-pre.txt', "r")
-print "The file test-pre.txt is as follows"
+outfile = open('lists/scam.txt', "w")
+infile = open('lists/scam-pre.txt', "r")
+print("The file test-pre.txt is as follows")
 for line in infile:
-    print line
+    print(line) #line
     if line not in lines_seen:  # not a duplicate
         outfile.write(line)
         lines_seen.add(line)
 outfile.close()
-print "The file lists/crypto.txt is as follows"
-for line in open('lists/crypto.txt', "r"):
-    print line
-os.remove("lists/crypto-pre.txt")
+print("The file lists/scam.txt is as follows")
+for line in open('lists/scam.txt', "r"):
+    print(line) #line
+os.remove("lists/scam-pre.txt")
