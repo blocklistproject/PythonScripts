fc7bc2b2d2353da0eafd52247f7ccb59f257112f
slimchahla <12842269+slimchahla@users.noreply.github.com>
2020-10-21 13:37:38 -0700

    Cleaned up summed.py

--- summed.py
+++ summed.py
@@ -1,26 +1,44 @@
-# Set cwd to file location
-os.chdir(os.path.dirname(os.path.realpath(__file__)))
+import glob
+from typing import Generator
 
-# Get every .txt file in folder (except output file)
-txt_files = [f for f in glob.glob("*.txt")]
-txt_files.remove("everything.txt")
+OUTPUT_FILE = 'everything.txt'
 
-# For non-duplicates lines + count + write
-read_lines = set()
 
-# Info text to go on top, formattable at "Total number of network filters"
-info_text = "\n#------------------------------------[UPDATE]--------------------------------------\n# Title: The Block List Project\n# Expires: 1 day\n# Homepage: https://blocklist.site\n# Help: https://github.com/blocklistproject/lists/wiki/\n# License: https://unlicense.org\n# Total number of network filters: {}\n#------------------------------------[SUPPORT]-------------------------------------\n# You can support by:\n# - reporting false positives\n# - making a donation: https://paypal.me/blocklistproject\n#-------------------------------------[INFO]---------------------------------------\n#\n# Summed list\n#------------------------------------[FILTERS]-------------------------------------\n"
+def read_files(files: Generator[str, None, None]) -> str:
+    for f in files:
+        # Skip output file
+        if f != OUTPUT_FILE:
+            for line in open(f, 'r'):
+                # Skip comments
+                if line[0] != '#':
+                    yield line
+
+
+def get_header(line_count: int) -> str:
+    # Info text to go on top, format at "Total number of network filters"
+    return ('#------------------------------------[UPDATE]--------------------------------------\n'
+            '# Title: The Block List Project\n'
+            '# Expires: 1 day\n'
+            '# Homepage: https://blocklist.site\n'
+            '# Help: https://github.com/blocklistproject/lists/wiki/\n'
+            '# License: https://unlicense.org\n'
+            f'# Total number of network filters: {line_count}\n'
+            '#------------------------------------[SUPPORT]-------------------------------------\n'
+            '# You can support by:\n'
+            '# - reporting false positives\n'
+            '# - making a donation: https://paypal.me/blocklistproject\n'
+            '#-------------------------------------[INFO]---------------------------------------\n'
+            '#\n'
+            '# Summed list\n'
+            '#------------------------------------[FILTERS]-------------------------------------\n')
+
+
+# Get every .txt file in folder
+txt_files = (f for f in glob.glob('*.txt'))
 
 # Read all .txt-files and save non-duplicate lines
-for input_file in txt_files:
-    for line in open(input_file, "r"):
-        read_lines.add(line)
+lines_set = set(read_files(txt_files))
 
-# Count, format and sum text
-no_of_lines = len(read_lines)
-info_text = info_text.format(no_of_lines - 16)
-summed_lines = "".join(read_lines)
-
-with open("everything.txt", "w") as output_file:
-    output_file.write(info_text)
-    output_file.write(summed_lines)
+with open(OUTPUT_FILE, "w") as output_file:
+    output_file.write(get_header(len(lines_set)))
+    output_file.write(''.join(lines_set))
